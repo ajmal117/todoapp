c5f0417dfc1d2f2d1a311c48833bc5e81b2a5054
ajmal ali <ajmal.mzn95@gmail.com>
2024-01-17 01:16:02 +0530

    cors add

--- ba/server.js
+++ ba/server.js
@@ -8,13 +8,11 @@ const app = express();
 app.use(express.json());
 
 app.use(
-  cors(
-    cors({
-      origin: ["https://todoapp-9ypc.vercel.app"],
-      methods: ["POST", "GET", "PUT", "DELETE"],
-      credentials: true,
-    })
-  )
+  cors({
+    origin: ["https://todoapp-9ypc.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
 );
 
 let port = 7000;
